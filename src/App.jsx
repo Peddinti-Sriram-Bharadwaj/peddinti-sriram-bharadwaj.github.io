3b55b87d040cbc4235fc6cd4f80f278b8f1fc2dc
Sriram Bhdaradwaj <sriram9217@gmail.com>
2023-11-11 09:53:37 +0530

    sitemap included in build directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Navbar, Tech, Works, StarsCanvas, Hero} from './components';
 
+
 console.log('hi');
 const App = () => {
 
   return (
     <BrowserRouter>
+    <div>
+      <a href={process.env.PUBLIC_URL + '../public/sitemap.xml'}></a>
+    </div>
       <div className="relative z-0 bg-primary">
         <div className = 'bg-hero-pattern bg-cover bg-center bg-no-repeat'>
           <Navbar />
